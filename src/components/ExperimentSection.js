e23d442f3cc953b045bfe55b258935696e1a2e69
Dev Patel <devarshi.ap@gmail.com>
2022-05-07 12:51:52 -0400

    add textarea and define state

--- src/components/ExperimentSection.js
+++ src/components/ExperimentSection.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 export default function ExperimentSection() {
+    const [inputText, setInputText] = useState("");
+
+    const handleChange = (event) => {
+        setInputText(event.target.value);
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Text was submitted');
+    }
+
     return (
         <section className="section is-small">
             <h1 className="section-divider-title">Try It Out!</h1>
+
+            <form onSubmit={handleSubmit}>
+                <div className="field text-input-field">
+                    <div className="control">
+                        <textarea
+                            className="textarea is-medium is-link is-focused"
+                            placeholder="Paste Text Here"
+                            value={inputText}
+                            onChange={handleChange}
+                        ></textarea>
+                    </div>
+                    <input className="button is-primary is-light is-outlined is-fullwidth" type="submit" value="Submit" />
+                </div>
+            </form>
         </section>
     )
 }
