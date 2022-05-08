626d687a95c7ea9999054526f65184d2f749bcb9
Shams Minal Rahman <64226740+tekkado@users.noreply.github.com>
2022-05-07 20:01:33 -0400

    Updated pictures and text

--- src/components/AboutTile.js
+++ src/components/AboutTile.js
@@ -1,47 +1,52 @@
-import illustration from './illustrations/jumping.png';
+import illustration from "./illustrations/jumping.png";
+import illustration2 from "./illustrations/3d-what.png";
+import illustration3 from "./illustrations/likes.png";
 
 export default function AboutTiles() {
-    return (
-        <div className="tile is-ancestor" style={{marginTop: "3rem"}}>
-            <div className="tile is-vertical is-7">
-                <div className="tile">
-                    <div className="tile is-parent is-vertical">
-                        <article className="tile is-child notification frosted-bg">
-                            <p className="title">Vertical...</p>
-                            <p className="subtitle">Top tile</p>
-                        </article>
-                        
-                        <article className="tile is-child notification is-warning">
-                            <p className="title">...tiles</p>
-                            <p className="subtitle">Bottom tile</p>
-                        </article>
-                    </div>
-                    <div className="tile is-parent">
-                        <article className="tile is-child notification is-info">
-                            <p className="title">Middle tile</p>
-                            <p className="subtitle">With an image</p>
-                            <figure className="image is-4by3">
-                                <img src="https://bulma.io/images/placeholders/640x480.png" alt="something"/>
-                            </figure>
-                        </article>
-                    </div>
-                </div>
-                <div className="tile is-parent">
-                    <article className="tile is-child notification is-danger">
-                        <p className="title">Wide tile</p>
-                        <p className="subtitle">Aligned with the right tile</p>
-                        <div className="content">Content</div>
-                    </article>
-                </div>
-            </div>
-
-            <div className="tile is-parent jumping-tile">
-                <article className="tile is-child notification is-success">
-                    <div className="content">
-                        <img src={illustration} alt="jumping man"/>
-                    </div>
-                </article>
-            </div>
+  return (
+    <div className="tile is-ancestor" style={{ marginTop: "3rem" }}>
+      <div className="tile is-vertical is-7">
+        <div className="tile">
+          <div className="tile is-parent is-vertical">
+            <article className="tile is-child notification frosted-bg">
+              <p className="title">Ever wonder why</p>
+              <img src={illustration2} alt="girl on laptop" />
+              <p className="subtitle">
+                a YouTube video was received the way it did?
+              </p>
+            </article>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child notification is-info">
+              <p className="title">Empath is</p>
+              <p className="subtitle">your answer</p>
+              <figure className="image is-4by3">
+                <img src={illustration3} alt="sentiment" />
+              </figure>
+              <br></br>
+              <p style={{ textAlign: "center" }} className="subtitle">
+                Empath determines whether the video or text submitted is of good
+                sentiment or bad sentiment
+              </p>
+            </article>
+          </div>
         </div>
-    )
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-danger">
+            <p className="title">Wide tile</p>
+            <p className="subtitle">Aligned with the right tile</p>
+            <div className="content">Content</div>
+          </article>
+        </div>
+      </div>
+
+      <div className="tile is-parent jumping-tile">
+        <article className="tile is-child notification is-success">
+          <div className="content">
+            <img src={illustration} alt="jumping man" />
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 }
